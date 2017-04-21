e2db7581e197c8219886cbfe02de2ba756b94ac5
pirej <pirej30@yahoo.com>
2017-04-21 23:39:09 +0200

    wt88047: VoLTE support (1/2)
    Based on the tomato part from vendor_volte by Louis Popi

--- wt88047/wt88047-vendor.mk
+++ wt88047/wt88047-vendor.mk
@@ -42,4 +42,27 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtime_genoff
 
+PRODUCT_PACKAGES += \
+    ims \
+    imssettings \
+    imscmlibrary
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enalbed_by_hw=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
 $(call inherit-product, vendor/xiaomi/wt88047/wt88047-vendor-blobs.mk)
