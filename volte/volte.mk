c7559c0e1be09f3a62c1304d4fff910c294a7237
MkfsSion <myychina28759@gmail.com>
2018-05-31 23:15:29 +0800

    wt88047: Move properties to device tree and correct the path of overlay.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- volte/volte.mk
+++ volte/volte.mk
@@ -15,23 +15,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    net.lte.volte_call_capable=true \
-    persist.ims.enableADBLogs=1 \
-    persist.ims.enableDebugLogs=1 \
-    persist.radio.calls.on.ims=true \
-    persist.radio.csvt.enabled=false \
-    persist.radio.jbims=1 \
-    persist.radio.ROTATION_ENABLE=1 \
-    persist.radio.VT_ENABLE=1 \
-    persist.radio.VT_HYBRID_ENABLE=1 \
-    persist.radio.VT_USE_MDM_TIME=0 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.volte_enalbed_by_hw=1 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
-
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/volte/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/wingtech/volte/overlay
